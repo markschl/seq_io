1d9ccde1eaeb4290b48486dcf2c9d693efa20dc4
marcschl <ms201@gmx.net>
2017-10-22 23:08:56 +0200

    Add more tests, fix & reformat tests

--- tests/test.rs
+++ tests/test.rs
@@ -2,83 +2,100 @@
 
 extern crate seq_io;
 
-use seq_io::fasta::{self,Record};
-use seq_io::fastq::{self,Record as FastqRecord};
+use seq_io::fasta::{self, Record};
+use seq_io::fastq::{self, Record as FastqRecord};
 
 
 
 const FASTA: &'static [&'static [u8]; 11] = &[
-   b">id desc",
-   b"ACCGTAGGCT",
-   b"CCGTAGGCTG",
-   b"CGTAGGCTGA",
-   b"GTAGGCTGAA",
-   b"CCCC",
-   b">id2",
-   b"ATTGTTGTTT",
-   b"ATTGTTGTTT",
-   b"ATTGTTGTTT",
-   b"GGGG"
+    b">id desc",
+    b"ACCGTAGGCT",
+    b"CCGTAGGCTG",
+    b"CGTAGGCTGA",
+    b"GTAGGCTGAA",
+    b"CCCC",
+    b">id2",
+    b"ATTGTTGTTT",
+    b"ATTGTTGTTT",
+    b"ATTGTTGTTT",
+    b"GGGG",
 ];
 
 
 fn concat_lines(lines: &[&[u8]], terminator: &[u8], last: bool) -> Vec<u8> {
-   let mut out: Vec<_> = lines.iter()
+    let mut out: Vec<_> = lines
+        .iter()
         .flat_map(|s| s.iter().chain(terminator))
         .cloned()
         .collect();
-   if !last {
-      let l = out.len();
-      out.truncate(l - terminator.len());
-   }
-   out
+    if !last {
+        let l = out.len();
+        out.truncate(l - terminator.len());
+    }
+    out
 }
 
 #[test]
 fn test_fasta_reader() {
-   let expected = [
-      (Ok("id"), Some(Ok("desc")), (1, 6)),
-      (Ok("id2"), None, (7, 11))
-   ];
-   let lterms: [&[u8]; 2] = [b"\n", b"\r\n"];
+    let expected = [
+        (Ok("id"), Some(Ok("desc")), (1, 6)),
+        (Ok("id2"), None, (7, 11)),
+    ];
+    let lterms: [&[u8]; 2] = [b"\n", b"\r\n"];
 
-   // try different line endings
-   for t in lterms.into_iter() {
+    // try different line endings
+    for t in lterms.into_iter() {
+        let fasta = concat_lines(FASTA, *t, false);
+        let exp_seqs: Vec<_> = expected
+            .iter()
+            .map(|&(_, _, (start, end))| {
+                (
+                    // raw sequence
+                    concat_lines(&FASTA[start..end], *t, false),
+                    // concatenated sequence
+                    FASTA[start..end].concat().to_vec(),
+                )
+            })
+            .collect();
 
-      let fasta = concat_lines(FASTA, *t, true);
-      let exp_seqs: Vec<_> = expected.iter().map(|&(_, _, (start, end))| (
-         // raw sequence
-         concat_lines(&FASTA[start..end], *t, false),
-         // concatenated sequence
-         FASTA[start..end].concat().to_vec()
-      )).collect();
+        // try different initial capacities to test
+        // buffer growing feature
+        for cap in 3..100 {
+            let mut exp_iter = expected.iter().zip(&exp_seqs);
+            let mut reader = fasta::Reader::with_capacity(fasta.as_slice(), cap);
+            while let Some((&(ref id, ref desc, _), &(ref raw_seq, ref seq))) = exp_iter.next() {
+                let record = reader
+                    .next()
+                    .unwrap()
+                    .expect(&format!("Error reading record at cap. {}", cap));
 
-      // try different initial capacities to test
-      // buffer growing feature
-      for cap in 3 .. 100 {
-         let mut exp_iter = expected.iter().zip(&exp_seqs);
-         let mut reader = fasta::Reader::with_capacity(fasta.as_slice(), cap);
-          while let Some((&(ref id, ref desc, _), &(ref raw_seq, ref seq))) = exp_iter.next() {
-              let record = reader.next().unwrap().expect(&format!("Error reading record at cap. {}", cap));
+                assert_eq!(record.id(), *id, "ID mismatch at cap. {}", cap);
+                assert_eq!(record.desc(), *desc, "desc mismatch at cap. {}", cap);
+                assert_eq!(
+                    record.seq(),
+                    raw_seq.as_slice(),
+                    "raw seq mismatch at cap. {}",
+                    cap
+                );
+                assert_eq!(
+                    record.owned_seq().as_slice(),
+                    seq.as_slice(),
+                    "seq mismatch at cap. {}",
+                    cap
+                );
 
-              assert_eq!(record.id(), *id, "ID mismatch at cap. {}", cap);
-              assert_eq!(record.desc(), *desc, "desc mismatch at cap. {}", cap);
-              assert_eq!(record.seq(), raw_seq.as_slice(), "raw seq mismatch at cap. {}", cap);
-              assert_eq!(record.owned_seq().as_slice(), seq.as_slice(), "seq mismatch at cap. {}", cap);
-
-              let owned = record.to_owned_record();
-              assert_eq!(owned.id(), *id, "ID mismatch at cap. {}", cap);
-              assert_eq!(owned.desc(), *desc, "desc mismatch at cap. {}", cap);
-              assert_eq!(owned.seq(), seq.as_slice(), "seq mismatch at cap. {}", cap);
-
-          }
-      }
-   }
+                let owned = record.to_owned_record();
+                assert_eq!(owned.id(), *id, "ID mismatch at cap. {}", cap);
+                assert_eq!(owned.desc(), *desc, "desc mismatch at cap. {}", cap);
+                assert_eq!(owned.seq(), seq.as_slice(), "seq mismatch at cap. {}", cap);
+            }
+        }
+    }
 }
 
 #[test]
 fn test_fasta_invalid_start() {
-    let mut reader = fasta::Reader::new(&b"id\nATGC"[..]);
+    let mut reader = fasta::Reader::new(&b"id\nATGC\n"[..]);
     let rec = reader.next().unwrap();
     assert!(rec.is_err() && format!("{}", rec.err().unwrap()).contains("Expected > at file start"));
 }
@@ -90,11 +107,39 @@ fn test_fasta_truncated() {
     assert!(rec.is_err() && format!("{}", rec.err().unwrap()).contains("Unexpected end of input"));
 }
 
-
 #[test]
 fn test_fasta_none_after_err() {
-    let mut reader = fasta::Reader::new(&b"id\nATGC"[..]);
+    let mut reader = fasta::Reader::new(&b"id\nATGC\n"[..]);
     assert!(reader.next().unwrap().is_err());
+    assert!(reader.next().is_none());
+}
+
+#[test]
+fn test_fasta_empty() {
+    let mut reader = fasta::Reader::new(&b""[..]);
+    assert!(reader.next().is_none());
+    let mut reader = fasta::Reader::new(&b"\n\n\n\n\n\n"[..]);
+    assert!(reader.next().is_none());
+}
+
+#[test]
+fn test_fasta_empty_lines_start() {
+    let mut reader = fasta::Reader::new(&b"\n\n\n\n\n\n\n\n\n>id\nATGC\n"[..]);
+    assert!(reader.next().unwrap().unwrap().id_bytes() == b"id");
+}
+
+#[test]
+fn test_fasta_empty_lines_end() {
+    let mut reader = fasta::Reader::new(&b">id\nATGC\n\n\n\n\n\n\n\n\n"[..]);
+    assert!(reader.next().unwrap().unwrap().id_bytes() == b"id");
+    assert!(reader.next().is_none());
+}
+
+#[test]
+fn test_fasta_no_newline_end() {
+    // up to 3 newlines at end possible
+    let mut reader = fasta::Reader::new(&b">id\nATGC"[..]);
+    assert!(reader.next().unwrap().unwrap().id_bytes() == b"id");
     assert!(reader.next().is_none());
 }
 
@@ -108,44 +153,58 @@ ATGC
 @id2
 ATGC
 +
-~~~~";
+~~~~
+";
 
 
 #[test]
 fn test_fastq_reader() {
-   let expected = [
-      (Ok("id"), Some(Ok("desc")), b"ATGC", b"~~~~"),
-      (Ok("id2"), None, b"ATGC", b"~~~~")
-   ];
+    let expected = [
+        (
+            Ok("id"),
+            Some(Ok("desc")),
+            &b"id desc"[..],
+            b"ATGC",
+            b"~~~~",
+        ),
+        (Ok("id2"), None, &b"id2"[..], b"ATGC", b"~~~~"),
+    ];
 
-   // try different initial capacities to test
-   // buffer growing feature
-   for cap in 3 .. 100 {
-      let mut exp_iter = expected.iter();
-      let mut reader = fastq::Reader::with_capacity(FASTQ, cap);
-      while let Some(&(ref id, ref desc, ref seq, ref qual)) = exp_iter.next() {
-           let record = reader.next().unwrap().expect(&format!("Error reading record at cap. {}", cap));
+    // try different initial capacities to test
+    // buffer growing feature
+    for cap in 3..100 {
+        let mut exp_iter = expected.iter();
+        let mut reader = fastq::Reader::with_capacity(FASTQ, cap);
+        while let Some(&(id, desc, head, seq, qual)) = exp_iter.next() {
+            let record = reader
+                .next()
+                .unwrap()
+                .expect(&format!("Error reading record at cap. {}", cap));
 
-           assert_eq!(record.id(), *id, "ID mismatch at cap. {}", cap);
-           assert_eq!(record.desc(), *desc, "desc mismatch at cap. {}", cap);
-           assert_eq!(record.seq(), *seq, "seq at cap. {}", cap);
-           assert_eq!(record.qual(), *qual, "qual mismatch at cap. {}", cap);
+            assert_eq!(record.id(), id, "ID mismatch at cap. {}", cap);
+            assert_eq!(record.desc(), desc, "desc mismatch at cap. {}", cap);
+            assert_eq!(record.head(), head, "head mismatch at cap. {}", cap);
+            assert_eq!(record.seq(), seq, "seq at cap. {}", cap);
+            assert_eq!(record.qual(), qual, "qual mismatch at cap. {}", cap);
 
-           let owned = record.to_owned_record();
-           assert_eq!(owned.id(), *id, "ID mismatch at cap. {}", cap);
-           assert_eq!(owned.desc(), *desc, "desc mismatch at cap. {}", cap);
-           assert_eq!(owned.seq(), *seq, "seq at cap. {}", cap);
-           assert_eq!(owned.qual(), *qual, "qual mismatch at cap. {}", cap);
-       }
-   }
+            let owned = record.to_owned_record();
+            assert_eq!(owned.id(), id, "ID mismatch at cap. {}", cap);
+            assert_eq!(owned.desc(), desc, "desc mismatch at cap. {}", cap);
+            assert_eq!(owned.head(), head, "head mismatch at cap. {}", cap);
+            assert_eq!(owned.seq(), seq, "seq at cap. {}", cap);
+            assert_eq!(owned.qual(), qual, "qual mismatch at cap. {}", cap);
+        }
+    }
 }
 
 
 #[test]
 fn test_fastq_invalid_start() {
-    let mut reader = fastq::Reader::new(&b"id\nATGC"[..]);
+    let mut reader = fastq::Reader::new(&b"id\nATGC\n+\n~~~~"[..]);
     let rec = reader.next().unwrap();
-    assert!(rec.is_err() && format!("{}", rec.err().unwrap()).contains("Expected '@' but found 'i'"));
+    assert!(
+        rec.is_err() && format!("{}", rec.err().unwrap()).contains("Expected '@' but found 'i'")
+    );
 }
 
 #[test]
@@ -166,7 +225,9 @@ fn test_fastq_unequal() {
 fn test_fastq_no_sep() {
     let mut reader = fastq::Reader::new(&b"@id\nATGC\n~~~~\n@id2"[..]);
     let rec = reader.next().unwrap();
-    assert!(rec.is_err() && format!("{}", rec.err().unwrap()).contains("Expected '+' but found '~'"));
+    assert!(
+        rec.is_err() && format!("{}", rec.err().unwrap()).contains("Expected '+' but found '~'")
+    );
 }
 
 #[test]
@@ -176,7 +237,67 @@ fn test_fastq_none_after_err() {
     assert!(reader.next().is_none());
 }
 
+#[test]
+fn test_fastq_empty() {
+    let mut reader = fastq::Reader::new(&b""[..]);
+    assert!(reader.next().is_none());
+}
+
+#[test]
+fn test_fastq_empty_lines_end() {
+    // up to 3 newlines at end possible
+    let mut reader = fastq::Reader::new(&b"@id\nATGC\n+\n~~~~\n\n\n"[..]);
+    assert!(reader.next().unwrap().unwrap().id_bytes() == b"id");
+    assert!(reader.next().is_none());
+}
+
+#[test]
+fn test_fastq_no_newline_end() {
+    let mut reader = fastq::Reader::new(&b"@id\nATGC\n+\n~~~~"[..]);
+    assert!(reader.next().unwrap().unwrap().id_bytes() == b"id");
+    assert!(reader.next().is_none());
+}
+
+#[test]
+fn test_fastq_recset() {
+    let expected = [
+        (
+            Ok("id"),
+            Some(Ok("desc")),
+            &b"id desc"[..],
+            b"ATGC",
+            b"~~~~",
+        ),
+        (Ok("id2"), None, &b"id2"[..], b"ATGC", b"~~~~"),
+    ];
+    let mut rset = fastq::RecordSet::default();
+    let mut reader = fastq::Reader::new(FASTQ);
+    reader.read_record_set(&mut rset).unwrap().unwrap();
+    let mut rset_iter = rset.into_iter();
+    for &(id, desc, head, seq, qual) in expected.into_iter() {
+        let rec = rset_iter.next().unwrap();
+        assert_eq!(rec.id(), id);
+        assert_eq!(rec.desc(), desc);
+        assert_eq!(rec.head(), head);
+        assert_eq!(rec.seq(), seq);
+        assert_eq!(rec.qual(), qual);
+    }
+}
+
 // FASTA writing
+
+#[test]
+fn test_write_fasta() {
+    let fasta = concat_lines(FASTA, b"\n", true);
+    let mut reader = fasta::Reader::new(&fasta[..]);
+    let mut out = vec![];
+    while let Some(r) = reader.next() {
+        let r = r.unwrap();
+        println!("{:?}", r.seq());
+        r.write_wrap(&mut out, 10).unwrap();
+    }
+    assert_eq!(&out, &fasta);
+}
 
 #[test]
 fn test_fasta_write_head() {
