9097434b01bafbc4c1616473fc40f6be2babe315
markschl <ms201@gmx.net>
2017-12-14 22:49:06 +0100

    Improve bench scripts

--- scripts/run_bench.sh
+++ scripts/run_bench.sh
@@ -3,7 +3,7 @@
 # sudo cpupower frequency-set -u 2.2G
 
 mkdir -p bench_results
-scripts/bench.sh > bench_results/bench.txt
+scripts/bench.sh $@ > bench_results/bench.txt
 
 Rscript scripts/bench_analysis.R bench_results/bench.txt
 
