f9d7e4dc05b43b83eabb0c55901fa392f72d7595
Adrian Seyboldt <adrian.seyboldt@gmail.com>
2018-08-10 21:23:16 +0200

    Add cargo fuzz

---
+++ fuzz/fuzz_targets/single_thread.rs
@@ -0,0 +1,21 @@
+#![no_main]
+#[macro_use] extern crate libfuzzer_sys;
+extern crate seq_io;
+extern crate criterion;
+
+use std::io::Cursor;
+use seq_io::fastq::{Reader, Record};
+
+
+fuzz_target!(|data: &[u8]| {
+    let reader = Cursor::new(data);
+    let mut reader = Reader::new(reader);
+    let mut count: usize = 0;
+
+    while let Some(result) = reader.next() {
+        if let Ok(record) = result {
+            count += record.seq().len();
+        }
+    }
+    criterion::black_box(count);
+});
